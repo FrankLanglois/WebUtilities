9020f749a042110f8aca8a84be46320b4336c519
Frank <franklanglois@gmail.com>
2017-09-19 09:53:33 -0400

    Add project files.

---
+++ WebUtilities/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebUtilities.Startup))]
+namespace WebUtilities
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
